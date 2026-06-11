81dcca1047d076e7399ba69639766c8fa71d7c3d
Avi <avi@Mac.home>
2026-06-10 23:48:28 -0700

    MH config changed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.WORLD_CUP_CONFIG = {
   // Replace this with your deployed Apps Script Web App /snapshot URL when ready.
   // Leave as null to use the checked-in simulated sample data.
-  snapshotUrl: null
+  snapshotUrl: "https://script.google.com/macros/s/AKfycby1fpIRSYQBesJBkF1lJhpUx7dE04STEegeiaAiFhEM4FO2-Qs6E4x2Q_w9PiKUcX-pFg/exec"
 };
