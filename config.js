204117f8f28f94e7696a67b644dd557e621c4dc2
Avinash Nandakumar <31336290+avinashnandakumar@users.noreply.github.com>
2026-06-11 21:58:23 -0700

    Update snapshotUrl to new backend URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.WORLD_CUP_CONFIG = {
   // Replace this with your deployed Apps Script Web App /snapshot URL when ready.
   // Leave as null to use the checked-in simulated sample data.
-  // change
-  snapshotUrl: "https://script.google.com/macros/s/AKfycby1fpIRSYQBesJBkF1lJhpUx7dE04STEegeiaAiFhEM4FO2-Qs6E4x2Q_w9PiKUcX-pFg/exec"
+  // changed to roast squad backend
+  snapshotUrl: "https://script.google.com/macros/s/AKfycbzlaQH0CaUKOO1mVADoKGXaJECF3T-ONOmFwLzayFqy6XxqszUqgvxVkj9TYRYP7PnD/exec"
 };
